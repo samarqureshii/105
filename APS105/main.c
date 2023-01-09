448d26c79399a5058f6a93f7277e30384b45c154
samarqureshii <samarq713@gmail.com>
2023-01-08 19:13:10 -0500

    Initial Commit

---
+++ APS105/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  APS105
+//
+//  Created by Samar Qureshi on 2023-01-08.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
