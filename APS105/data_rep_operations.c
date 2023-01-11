92605aac286c8f6878e11bb10d7fa515181a29ee
samarqureshii <samarq713@gmail.com>
2023-01-11 09:19:47 -0500

    build failed?

---
+++ APS105/data_rep_operations.c
@@ -0,0 +1,24 @@
+//
+//  data_rep_operations.c
+//  APS105
+//
+//  Created by Samar Qureshi on 2023-01-11.
+//
+
+#include <stdio.h>
+
+int main(void){
+  // Declare variables
+  const double InchesToCm = 2.54;
+  double inputInches, outputCm;
+
+  // Prompt user for input
+  printf("Enter the number of inches to convert to cm: ");
+  scanf("%lf", &inputInches);
+  // Convert inches to centimeters
+  outputCm = inputInches * InchesToCm;
+ 
+  // Display output in 2 decimal places
+  printf("The number of centimeters is %.2lf\n", outputCm);
+  return 0;
+}
