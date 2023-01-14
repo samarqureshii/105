c153aae4ab3b05136e032f27a2dec95daa143f42
samarqureshii <samarq713@gmail.com>
2023-01-14 18:29:55 -0500

    working version lab1

---
+++ APS105/labs/lab1.c
@@ -0,0 +1,68 @@
+//
+//  lab1.c
+//  APS105
+//
+//  Created by Samar Qureshi on 2023-01-14.
+//
+
+#include <stdio.h>
+
+int lab1_q1(void) {
+    // insert code here...
+    printf("C uses escape sequences for a variety of purposes. \n Some common ones are: \n");
+    printf("\t to print \", use \\ ");
+    printf("\" \n");
+    printf("\t to print  \\, use  \\");
+    printf("\\ \n");
+    printf("\t to jump to a new line, use \\");
+    printf("n \n");
+    return 0;
+}
+
+//Enter the conversion rate: 0.17<enter>
+//Enter the amount to be converted (in foreign currency): 245.00<enter>
+//
+//The amount in Canadian Dolars is: 41.65
+
+int lab1_q2(void) {
+    double rate, amount;
+    
+    printf("Enter the conversion rate: ");
+    scanf("%lf", &rate);
+    
+    printf("\nEnter the amount to be converted (in foreign currency): ");
+    scanf("%lf", &amount);
+    
+    amount = amount * rate;
+    
+    printf("\nThe amount in Canadian Dollars is: %.2lf\n", amount);
+    
+    return 0;
+}
+
+//Enter number to convert to base 2: 13<enter>
+//The four digits of that number are as follows:
+//Most significant digit: 1
+//Next digit: 1
+//Next digit: 0
+//Least significant digit: 1
+
+int lab2_q3(void){
+    int num, d1, d2, d3, d4;
+    printf("Enter number to convert to base 2: ");
+    scanf("%d", &num);
+    
+    d4 = (num%2);
+    d3 = (num/2)%2;
+    d2 = ((num/2)/2)%2;
+    d1 = (((num/2)/2)/2)%2;
+    
+    printf("The four digits of that number are as follows:");
+    printf("\nMost significant digit: %d", d1);
+    printf("\nNext digit: %d", d2);
+    printf("\nNext digit: %d", d3);
+    printf("\nLeast significant digit: %d", d4);
+
+    
+    return 0;
+}
--- APS105/main.c
+++ APS105/main.c
@@ -3,7 +3,21 @@
 #include <stdio.h>
 
 int main(void) {
-    // insert code here...
-    printf("Hello World!\n");
+    int num, d1, d2, d3, d4;
+    printf("Enter number to convert to base 2: ");
+    scanf("%d", &num);
+    
+    d4 = (num%2);
+    d3 = (num/2)%2;
+    d2 = ((num/2)/2)%2;
+    d1 = (((num/2)/2)/2)%2;
+    
+    printf("The four digits of that number are as follows:");
+    printf("\nMost significant digit: %d", d1);
+    printf("\nNext digit: %d", d2);
+    printf("\nNext digit: %d", d3);
+    printf("\nLeast significant digit: %d", d4);
+
+    
     return 0;
 }
