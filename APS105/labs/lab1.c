661234d446511a3b225f53ffea372a4f250edd2b
samarqureshii <samarq713@gmail.com>
2023-01-16 13:15:39 -0500

    added q4

--- APS105/labs/lab1.c
+++ APS105/labs/lab1.c
@@ -66,3 +66,34 @@ int lab2_q3(void){
     
     return 0;
 }
+
+int lab3_q3(void){
+    const double CmPerInch = 2.54;
+    const double CmPerMetre = 100.00;
+    const int InchesPerFoot = 12;
+    const int InchesPerYard = 36;
+    
+    double distance;
+    printf("Please provide a distance in metres: ");
+    scanf("%lf", &distance);
+    
+    double distanceInInches = distance * CmPerMetre / CmPerInch;
+//    printf("%lf is the distance in inches \n", distanceInInches);
+    
+    // truncate fractional part to get # of inches
+    int inches = distanceInInches;
+    distanceInInches = distanceInInches - inches;
+    
+    int yards = inches / InchesPerYard;
+    
+    // how many inches are left after extracting yards
+    inches = inches % InchesPerYard;
+    int feet = inches / InchesPerFoot;
+    
+    // how many inches are left after extracting feet
+    inches = inches % InchesPerFoot;
+    
+    printf("%d yards, %d feet, %d inches, %.2f inches remainder\n", yards, feet, inches, distanceInInches);
+    
+    return 0;
+}
