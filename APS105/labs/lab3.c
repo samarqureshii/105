1924c43867ac73240e70124a6d3be691f30fe4e2
samarqureshii <samarq713@gmail.com>
2023-02-04 10:05:05 -0500

    q1 cursed but finally working

---
+++ APS105/labs/lab3.c
@@ -0,0 +1,124 @@
+//
+//  lab3.c
+//  APS105
+//
+//  Created by Samar Qureshi on 2023-02-01.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int biggestDigit(int num){ //find the largest digit in any number
+    int large = 0;
+    
+    while(num!=0){
+        int n = num % 10; //extracting the digits
+        large = fmax(n, large);
+        num = num/10;
+    }
+    
+    return large;
+}
+
+int convertToDecimal(int num, int base){ //converts a number in any base from 2-10 TO decimal (base 10)
+    int ans = 0, power = 1;
+    
+    while(num!=0){ //iterate through all the digits and multiply them by their respective weights
+        int r = num%10;
+        num/=10;
+        
+        ans+=r*power;
+        power*=base;
+        
+    }
+    
+    return ans; //returns the number in base 10
+}
+
+int convertFromDecimal(int num, int base){ //converts a number FROM decimal to any base (from 2-10)
+    int ans = 0;
+    int power = 0;
+    int rem;
+    
+    while(num>0){
+        rem = num%base;
+        ans+=rem*pow(10, power);
+        num/=base;
+        power++;
+    }
+    
+    return ans; //returns the number in the desired output base
+}
+
+int lab3_q1(void){ //q1
+    char op = 0;
+    int inBase, num1, num2, outBase;
+    
+    while (op!= '$'){
+        printf("Give input ($ to stop): ");
+        scanf(" %c %d %d %d %d", &op, &inBase, &num1, &num2, &outBase);
+        
+        //TESTING
+//        printf("in base %d", inBase);
+//        printf("\nout base %d", outBase);
+//        printf("\nbiggest digit 1: %d", biggestDigit(num1));
+//        printf("\n biggest digit 2: %d \n", biggestDigit(num2));
+//
+        if(op!= '+' && op!= '-' && op!= '*' && op!='/'){ //if operator is not valid
+            //printf("Operator: %c", op);
+            
+            if(op == '$'){
+                break;
+            }
+            printf("Invalid operator\n");
+        }
+
+        else if(inBase<2 || outBase<2 || inBase >10 || outBase>10){ //invalid input/output base
+            printf("Invalid base\n");
+        }
+
+        else if(biggestDigit(num1) >= inBase || biggestDigit(num2) >= inBase){ //invalid operand
+            //check to see if any of the digits are >= the base entered
+            printf("Invalid digits in operand\n");
+        }
+        
+        else{ //if the input is valid
+            //display LH side of operation
+            printf("%d %c %d (base %d) = ", num1, op, num2, inBase);
+            
+            //convert both numbers from inBase to base 10
+            num1 = convertToDecimal(num1, inBase);
+            num2 = convertToDecimal(num2, inBase);
+            
+            //display middle portion of operation
+            
+            printf("%d %c %d = ", num1, op, num2);
+            
+            //perform operation in base 10
+            int postOp = 0; //the concat. of the two numbers post-operation
+            
+            if(op == '+'){
+                postOp  = num1+num2;
+            }
+            
+            else if(op == '-'){
+                postOp = num1-num2;
+            }
+            
+            else if(op == '*'){
+                postOp = num1*num2;
+            }
+            
+            else if(op == '/'){
+                postOp = num1/num2;
+            }
+            
+        
+            //convert the result of the operation to the outBase
+            printf("%d (base %d)\n", convertFromDecimal(postOp, outBase), outBase);
+        }
+
+    }
+    
+}
+
